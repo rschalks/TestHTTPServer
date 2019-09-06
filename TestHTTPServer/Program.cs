479a1286ff004acc85cffed5370efc2c2efae9bb
Recurracy <r.schalks@hotmail.com>
2019-09-06 15:44:39 +0200

    program.cs

---
+++ TestHTTPServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LWHTTP.Server;
+
+namespace LWHTTP
+{
+    public class Program
+    {
+        public static HTTPServer Server { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Server = new HTTPServer();
+            Server.Initialize();
+        }
+    }
+}
