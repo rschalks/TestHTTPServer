be6e15f708c1e26a14cc0825c7c4c5e98e6b105f
Recurracy <r.schalks@hotmail.com>
2019-09-06 15:45:24 +0200

    Remove IHTTPHeader interface, static function to parse a HTTP header from a string

--- TestHTTPServer/HTTPCommon/HTTPHeaders/HTTPHeader.cs
+++ TestHTTPServer/HTTPCommon/HTTPHeaders/HTTPHeader.cs
@@ -2,22 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 using LWHTTP.HTTPCommon;
+using System.Reflection;
+using System.Linq;
+using System.Net;
 
 namespace LWHTTP.HTTPCommon.HTTPHeaders
 {
     /// <summary>
-    /// This interface exists to allow you to put generic items into a generic IENumerable.
+    /// This class contains information about a HTTP Header. Can not be created by itself.
     /// </summary>
-    public interface IHTTPHeader
-    {
-        Type ContentType { get; }
-    }
-
-    /// <summary>
-    /// This class contains information about a HTTP Header.
-    /// </summary>
-    /// <typeparam name="TFormat">Generic type, where TFormat must be an instance of IFormattable.</typeparam>
-    public abstract class HTTPHeader<TFormat> : IHTTPHeader where TFormat : IFormattable
+    public class HTTPHeader
     {
         /// <summary>
         /// The title of this header.
@@ -26,11 +20,16 @@ namespace LWHTTP.HTTPCommon.HTTPHeaders
         /// <summary>
         /// The content of this header.
         /// </summary>
-        public TFormat Content { get; set; }
+        public object Content { get; set; }
         /// <summary>
         /// The Type of the Content.
         /// </summary>
-        public Type ContentType { get { return typeof(TFormat); } }
+        public Type ContentType { get; protected set; }
+
+        public HTTPHeader()
+        {
+            this.ContentType = typeof(String);
+        }
 
         /// <summary>
         /// Returns this Header as an HTTP recognized string.
@@ -41,24 +40,45 @@ namespace LWHTTP.HTTPCommon.HTTPHeaders
             return String.Format("{0}: {1}", this.Title, this.Content.ToString());
         }
 
-        /// <summary>
-        /// Returns this Header as an HTTP recognized string. 
-        /// </summary>
-        /// <param name="format">The format to pass onto the content ToString</param>
-        /// <param name="formatProvider">The FormatProvider to pass onto the content ToString</param>
-        /// <returns>string</returns>
-        public virtual string ToString(string format, IFormatProvider formatProvider)
+        public virtual void ParseContent(string str)
         {
-            return string.Format("{0}: {1}", this.Title, this.Content.ToString(format, formatProvider));
+            this.Content = str;
+        }
+
+        public static HTTPHeader Parse(string str)
+        {
+            int colonIndex = str.IndexOf(':');
+            string headerStr = str.Substring(0, colonIndex);
+            string contentStr = str.Substring(colonIndex + 2);
+            Console.WriteLine("Received header - {0}", str);
+            HTTPHeader ret = null;
+            // use reflection to get the correct header
+            var headerType = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(t => t.GetTypes())
+                .FirstOrDefault(t => (t.IsClass && t.Namespace == "LWHTTP.HTTPCommon.HTTPHeaders" && t.Name == String.Format("HTTP{0}Header", headerStr)));
+            if (headerType != null)
+            {
+                ret = (HTTPHeader)Activator.CreateInstance(headerType);
+                ret.ParseContent(contentStr);
+                Console.WriteLine("Header is of known type {0}", headerType.FullName);
+                return ret;
+            }
+            else
+            {
+                ret = new HTTPHeader();
+                ret.Title = headerStr;
+                ret.Content = contentStr;
+            }
+
+            return ret;
         }
     }
     
-    public class HTTPConnectionHeader : HTTPHeader<HTTPConnection>
+    public class HTTPConnectionHeader : HTTPHeader
     {
-        HTTPConnection ConnectionType { get; set; }
-
         public HTTPConnectionHeader()
         {
+            this.ContentType = typeof(HTTPConnection);
             this.Title = "Connection";
         }
 
@@ -87,6 +107,44 @@ namespace LWHTTP.HTTPCommon.HTTPHeaders
                 default:
                     return "";
             }
+        }
+
+        public override void ParseContent(string str)
+        {
+            str = str.Replace("-", "").ToLower();
+            string[] enumNames = Enum.GetNames(typeof(HTTPConnection));
+            for (int i = 0; i < enumNames.Count(); i++)
+            {
+                if (enumNames[i].ToLower() == str)
+                {
+                    this.Content = (HTTPConnection)i;
+                    return;
+                }
+            }
+        }
+    }
+
+    public class HTTPHostHeader : HTTPHeader
+    {
+        public int Port { get; set; }
+
+        public HTTPHostHeader()
+        {
+            this.ContentType = typeof(IPAddress);
+            this.Title = "Host";
+            this.Port = -1;
+        }
+
+        public override void ParseContent(string str)
+        {
+            string ipStr = str;
+            if (str.Contains(":"))
+            {
+                int colonIndex = str.IndexOf(':');
+                string portStr = str.Substring(colonIndex + 1);
+                Port = int.Parse(portStr);
+            }
+            this.Content = IPAddress.Parse(ipStr);
         }
     }
 }
