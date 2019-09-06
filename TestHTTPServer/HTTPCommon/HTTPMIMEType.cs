4bae20340d75dfabf87358688971d897ec87ed1a
Recurracy <r.schalks@hotmail.com>
2019-09-06 15:46:41 +0200

    HTTPMimeType added

---
+++ TestHTTPServer/HTTPCommon/HTTPMIMEType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestHTTPServer.HTTPCommon
+{
+    class HTTPMIMEType
+    {
+        String Type { get; set; }
+        string Subtype { get; set; } 
+        float Quality { get; set; }
+
+        public HTTPMIMEType()
+        {
+            this.Quality = -1;
+        }
+
+        public static HTTPMIMEType Parse(string str)
+        {
+            HTTPMIMEType typ = new HTTPMIMEType();
+            return null;
+        }
+    }
+}
